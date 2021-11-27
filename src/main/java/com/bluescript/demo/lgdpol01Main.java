9e3848a226a97474657811cdfd26549e300d36cd
bluebrain-ai <bluebrainenterprise@gmail.com>
2021-11-27 15:16:44 +0530

    first commit

---
+++ src/main/java/com/bluescript/demo/lgdpol01Main.java
@@ -0,0 +1,13 @@
+package com.bluescript.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lgdpol01Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(lgdpol01Main.class, args);
+    }
+
+}
